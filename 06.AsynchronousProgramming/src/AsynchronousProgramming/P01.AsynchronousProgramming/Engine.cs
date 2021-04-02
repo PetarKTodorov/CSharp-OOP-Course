32082fa067aa83a0670bfe9fad4038b77f6e4f2f
PetarKTodorov <PetarKTodorov@gmail.com>
2021-04-02 17:23:39 +0300

    add AsynchronousProgramming

---
+++ 06.AsynchronousProgramming/src/AsynchronousProgramming/P01.AsynchronousProgramming/Engine.cs
@@ -0,0 +1,49 @@
+﻿namespace P01.AsynchronousProgramming
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class Engine
+    {
+        public async Task RunAsync()
+        {
+            //Console.Write("Enter a number: ");
+            //int endNumber = int.Parse(Console.ReadLine());
+
+            //PrintNumbersInRange(1, endNumber);
+
+            //var task = Task.Run(() => PrintNumbersInRange(endNumber, endNumber + 20));
+
+            //Console.WriteLine("Done.");
+
+            //task.GetAwaiter().GetResult();
+
+            int contentLength = await GetUrlContentLengthAsync();
+
+            Console.WriteLine(contentLength);
+        }
+
+        private static void PrintNumbersInRange(int startNumber, int endNumber)
+        {
+            for (int number = startNumber; number <= endNumber; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static async Task<int> GetUrlContentLengthAsync()
+        {
+            var client = new HttpClient();
+
+            Task<string> getStringTask1 = client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+            PrintNumbersInRange(10, 15);
+
+            string contents = await getStringTask1;
+
+            return contents.Length;
+        }
+
+    }
+}
---
+++ 06.AsynchronousProgramming/src/AsynchronousProgramming/P01.AsynchronousProgramming/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace P01.AsynchronousProgramming
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var engine = new Engine();
+
+            engine.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
